239aa29cfff2d673901814d9bd2dcba4648a0328
Miguel Vilá <miguelvilag@gmail.com>
2013-03-07 14:54:58 -0500

    Agregados datos a las figuras de las imagenes

--- main.m
+++ main.m
@@ -3,6 +3,12 @@ X = I(:);
 K = 4;
 [centroids, idx] = kMeans(X, K);
 I_out = reshape(im2uint8(centroids(idx,:)), size(I));
-imshow(I);
-imshow(I_out);
+
+figure;imshow(im2uint8(I));
+iminfo = impixelinfo;
+set(iminfo,'Position',[15 22 300 20]);
+figure;imshow(I_out);
+iminfo = impixelinfo;
+set(iminfo,'Position',[15 22 300 20]);
+
 imwrite(I_out, 'out.png');
