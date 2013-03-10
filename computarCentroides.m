b75d78d570e7cfde5f4c3d84092a3587891e07ca
Miguel Vilá <miguelvilag@gmail.com>
2013-03-10 02:12:12 -0500

    Revert "Quitados comentarios (Deshacer este commit)"
    This reverts commit 4bce27c002b97201a2b36b4b6c0bd70d5e184107.

--- computarCentroides.m
+++ computarCentroides.m
@@ -1,8 +1,15 @@
 function centroides = computarCentroides(X, idx, K)
+% A partir de la asignacion de centroides computa los nuevos centroides
 
 for k=1:K,
+	% Encuentra los indices de los elementos que fueron asignados al k-esimo cluster
 	v = find(idx==k);
-	centroides(k,:) = sum(X(v,:))/length(v);
+	
+	% Obtiene los elementos a partir de los indices 
+	Xs = X(v,:);
+	
+	% El nuevo centroide es el promedio de los elementos asignados al k-esimo cluster
+	centroides(k,:) = sum(Xs)/length(v);
 end;
 
 end
--- construirMatrizDeEntrada.m
+++ construirMatrizDeEntrada.m
@@ -1,9 +1,13 @@
 function X = construirMatrizDeEntrada(I)
-x = size(I,1);
-y = size(I,2);
+
+[x y] = size(I);
+% Inicializa una matriz de elementos que va a representar todos los pixeles de la imagen
 X = zeros(x*y, 3);
+
 for j=1:y
     for i=1:x
+		% Cada elemento es un vector fila de tres dimensiones que tiene el nivel de gris del pixel y
+		% su posicion en x y y
         X(i+(j-1)*x,:)=[I(i,j) i j];
     end
 end
